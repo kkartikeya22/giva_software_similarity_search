78907c030b13f92ebffe72604f7958775f44c708
kkartikeya22 <120785187+kkartikeya22@users.noreply.github.com>
2025-03-22 22:29:38 +0530

    Update locustfile.py

--- locustfile.py
+++ locustfile.py
@@ -3,7 +3,7 @@ from locust import HttpUser, task, between
 class SearchLoadTest(HttpUser):
     wait_time = between(1, 3)
     
-    host = "https://your-app-name.onrender.com"  
+    host = "https://giva-software-similarity-search.onrender.com"  
 
     @task
     def search_query(self):
